c6de816f01fd9cb03d0e400495136d55980fc125
AnastasiiaBlon <anastasiiablonskaia@gmail.com>
2024-11-27 22:35:12 +0300

    added resources directory with image added AutomationPracticeForm.java updated build.gradle with path to image

---
+++ src/test/java/tests/AutomationPracticeForm.java
@@ -0,0 +1,65 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AutomationPracticeForm {
+
+    @BeforeAll
+    static void beforeTestsStart() {
+        Configuration.browserSize = "1928x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#userEmail").setValue("johnDoe@gmail.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("8900000000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text("18")).click();
+        $("#userForm #subjectsInput").setValue("Arts");
+        $("#userForm .subjects-auto-complete__menu").click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("ams.jpg");
+        $("#currentAddress").setValue("USA");
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Lucknow").pressEnter();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table").$(byText("Student Name")).sibling(0).shouldHave(text("John Doe"));
+        $(".table").$(byText("Student Email")).sibling(0).shouldHave(text("johnDoe@gmail.com"));
+        $(".table").$(byText("Gender")).sibling(0).shouldHave(text("Female"));
+        $(".table").$(byText("Mobile")).sibling(0).shouldHave(text("8900000000"));
+        $(".table").$(byText("Date of Birth")).sibling(0).shouldHave(text("18 May,1993"));
+        $(".table").$(byText("Subjects")).sibling(0).shouldHave(text("Arts"));
+        $(".table").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports, Reading"));
+        $(".table").$(byText("Picture")).sibling(0).shouldHave(text("ams.jpg"));
+        $(".table").$(byText("Address")).sibling(0).shouldHave(text("USA"));
+        $(".table").$(byText("State and City")).sibling(0).shouldHave(text("Uttar Pradesh Lucknow"));
+
+    }
+}
