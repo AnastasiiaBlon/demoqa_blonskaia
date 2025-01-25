f4ed49d717f3ea56afe49968714050a05ad7b4cf
AnastasiiaBlon <anastasiiablonskaia@gmail.com>
2025-01-25 19:22:38 +0300

    added attachments

--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -1,8 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -37,8 +37,11 @@ public class RegistrationRemoteTests {
     }
 
     @AfterEach
-    void afterEachTest() {
-        Selenide.closeWebDriver();
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
     @Test
